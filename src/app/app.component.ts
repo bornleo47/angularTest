7c4c0aa51104d6d3a2614899956e0bc2b348f255
Anuk Shakya <i11134@d2hs.com>
2018-08-24 04:58:02 -0700

    Angular Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'restApp';
+
+response:any;
+
+constructor(private http: HttpClient)
+{
+
 }
+
+ngOnInit(){
+  this.http.get("http://localhost:8080/empJson")
+  .subscribe((response)=>
+  {this.response=response;
+  console.log(response);
+})
+
+}
+
+}
+
